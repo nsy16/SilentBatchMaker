709d6389e8193ae5fa29d57ef7514144061d32a0
nsy16 <nrs084@gmail.com>
2018-06-29 12:31:35 +1200

    Add files via upload

--- compile.cmd
+++ compile.cmd
@@ -1,7 +1,7 @@
 if %PROCESSOR_ARCHITECTURE% == x86 set _autoit=C:\Program Files\AutoIt3\Aut2Exe\Aut2exe.exe
 if %PROCESSOR_ARCHITECTURE% == AMD64 set _autoit=C:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe.exe
 
-"%_autoit%" /in "%~dp0silentbatchmaker-melba2.au3" /out "%~dp0sbm.exe" /pack /x86
+"%_autoit%" /in "%~dp0silentbatchmaker-melba2.au3" /out "%~dp0sbm.exe" /pack /x86 /icon "%~dp0Rokey-Smooth-Metal-Msdos-batch-file.ico"
 
 
 
